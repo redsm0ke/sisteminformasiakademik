b106f81258f9e987661c0c7e2fa94f8436519177
Dianeme <redsm0kex@gmail.com>
2019-03-04 11:56:05 -0500

    Update db.php

--- assets/includes/db.php
+++ assets/includes/db.php
@@ -3,5 +3,5 @@ $host       =   "localhost";
 $user       =   "root";	
 $password   =   "";
 $database   =   "sia_db";
-$db 		= 	mysqli_connect($host, $user, $password, $database);
+$db         = 	mysqli_connect($host, $user, $password, $database);
 ?>
